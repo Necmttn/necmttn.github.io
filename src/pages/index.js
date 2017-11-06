cee0545abde0be1c6991aec948b638c0ed1ef001
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2017-11-06 23:50:16 +0800

    figured out how to handle hover issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import Link from 'gatsby-link'
 import styled from "styled-components"
 import { Bold } from '../components/Text'
 
+import coding from './gifs/coding.gif'
+
 const IndexPage = () => (
   <Wrapper>
     <Intro>
@@ -26,25 +28,44 @@ const Wrapper = styled.div`
 
 
 class TextBody extends React.Component {
-  state = {
-    neco: true,
-    work: false,
-    meetup: false,
+  constructor(props){
+    super(props)
+    this.state = {
+      code: false
+    }
   }
-  codingGif = () =>  (this.state.neco) ? (<img src='./static/coding.gif' />) : ''
+
+  mouseEnter(name) {
+    console.log('mouse enter', name)
+    this.setState({
+      [name]: true
+    })
+  }
+
+  mouseLeave(name) {
+    console.log('mouse leave', name)
+    this.setState({
+      [name]: false
+    })
+  }
+
   render() {
     return (
       <TextBodyContainer>
         <p>
           Hello <br/>
-          I am <bold> Necmettin Karakaya</bold> <br/>
-          self-tought software developer <br/>
-          Currently working at <a href="http://kiwiinc.net/"><bold>Kiwi Inc.</bold></a> <br/>
+          I am
+          <span className="neco">
+            Necmettin Karakaya
+          </span> <br/>
+
+          self-tought software <span onMouseEnter={() => this.mouseEnter('code')} onMouseLeave={() => this.mouseLeave('code')}> developer </span> <br/>
+          Currently working at <a href="http://kiwiinc.net/"><span className="Mesh">Mesh</span></a> <br/>
           building cross platform app for UAV devices.
         </p>
         <p> some highlighted text </p>
-        <div>
-          { this.codingGif() }
+        <div className="gifArea">
+            <GifWrapper src={coding} display={this.state.code}/>
         </div>
       </TextBodyContainer>
     )
@@ -54,9 +75,29 @@ class TextBody extends React.Component {
 
 
 
+const GifWrapper = styled.img`
+  display: ${props => props.display ? 'block' : 'none' }
+`
+
 const TextBodyContainer = styled.div`
   padding: 20px;
   max-width: 1100px;
+  display: flex;
+  align-items: flex-start;
+  flex-flow: column nowrap;
+  & .developer {
+    font-weight: 800;
+    & .coding {
+      position: absolute;
+      margin-left: 200px;
+      display: none;
+    }
+    &: hover {
+      & .coding {
+        display: block;
+      }
+    }
+  }
 `
 
 
