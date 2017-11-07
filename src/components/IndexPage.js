f4ca91ee52c64e490b21951499787ee783baa89e
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2017-11-08 02:08:59 +0800

    added some styling in to it

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import SplashScreen from './SplashScreen'
-
+import styled from 'styled-components'
 
 const Index = (props) => {
   const { langKey }  = props.pathContext;
   return (
-    <div>
+    <Intro>
       <SplashScreen currentLangKey={langKey}/>
-    </div>
+    </Intro>
   )
 }
 
+
+const Intro = styled.article`
+  color: #ffffff;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-flow: column nowrap;
+`
 export default Index
--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import coding from '../pages/gifs/coding.gif'
+import Link from 'gatsby-link'
 
 class SplashScreen extends React.Component {
   constructor(props) {
@@ -25,7 +26,7 @@ class SplashScreen extends React.Component {
   getWelcomeMsg = () => {
     return {
       en: (
-        <div>
+        <TextBodyContainer>
           <p>
             Hello, <br/>
             I am Necmettin Karakaya <br />
@@ -33,10 +34,10 @@ class SplashScreen extends React.Component {
             Currently working at <a href="http://kiwiinc.net/"><span className="Mesh">Mesh</span></a> <br/>
             building cross platform app for UAV devices. </p>
           <p> Life-long learner, pure geek </p>
-        </div>
+        </TextBodyContainer>
       ),
       zh: (
-        <div>
+        <TextBodyContainer>
           <p>
             你好, <br/>
             I am Necmettin Karakaya <br />
@@ -44,10 +45,10 @@ class SplashScreen extends React.Component {
             Currently working at <a href="http://kiwiinc.net/"><span className="Mesh">Mesh</span></a> <br/>
             building cross platform app for UAV devices. </p>
           <p> Life-long learner, pure geek </p>
-        </div>
+        </TextBodyContainer>
       ),
       tr: (
-        <div>
+        <TextBodyContainer>
           <p>
             Merhaba, <br/>
             I am Necmettin Karakaya <br />
@@ -55,19 +56,52 @@ class SplashScreen extends React.Component {
             Currently working at <a href="http://kiwiinc.net/"><span className="Mesh">Mesh</span></a> <br/>
             building cross platform app for UAV devices. </p>
           <p> Life-long learner, pure geek </p>
-        </div>
+        </TextBodyContainer>
       )
     };
   };
   render() {
     return (
-      <div>
+      <Section>
+        <Link to="/tr/">Turkce</Link>
+        <Link to="/en/">English</Link>
+        <Link to="/zh/">中文</Link>
         {this.getWelcomeMsg()[this.props.currentLangKey]}
-      </div>
+        <GifBodyContainer>
+          <GifWrapper src={coding} display={this.state.code}/>
+        </GifBodyContainer>
+      </Section>
     )
   }
 }
 
+
+
+const TextBodyContainer = styled.div`
+  padding: 20px;
+  max-width: 1100px;
+  width: 70vw;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-flow: column nowrap;
+`
+
+const Section = styled.section`
+  background: #1e2328;
+  width: 100%;
+  height: 95vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const GifBodyContainer = styled.div`
+  margin-left: auto;
+`
+const GifWrapper = styled.img`
+  display: ${props => props.display ? 'block' : 'none' }
+`
 
 const MagicWord = styled.span`
   border-bottom: 3px solid #9dffc8;
