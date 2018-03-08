59822a9a4cc3173569ac9b6c0f707d1faf1e5789
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2018-03-08 17:19:33 +0800

    ga added

--- packages/necmttn-gatsby/gatsby-config.js
+++ packages/necmttn-gatsby/gatsby-config.js
@@ -69,6 +69,18 @@ module.exports = {
         }
       }
     },
-    `gatsby-plugin-styled-components`
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-115374727-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+    },
   ],
 }
